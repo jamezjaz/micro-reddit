8004fd6d68454d0f53dfc942464fc7c96604b7ba
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-06-30 19:08:54 +0500

    Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-  has_many comments
-  belongs_to user
+  # has_many comments
+  # belongs_to user
+  validates :title, length: { maximum: 70 }, presence: true
+  validates :body, length: { maximum:200 }, presence: true
 end
