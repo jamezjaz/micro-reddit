d68a647f6fb3615a50ee2f56cf81f61693bba8ce
Jamezjaz <jamezjaz@gmail.com>
2020-06-30 13:39:17 -0500

    Updates validations and creates rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  # belongs_to posts
-  # belongs_to user
+  belongs_to :posts
+  belongs_to :user
   validates :body, length: { maximum: 70 }, presence: true
 end
